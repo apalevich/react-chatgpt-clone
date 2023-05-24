470875362783e53247b83092b444f2214cdcf7a8
apalevich <backstageofme@gmail.com>
2023-05-24 17:36:15 +0400

    Base layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 function App() {
   return (
     <div className="App">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Artyom</p>
+        </nav>
+      </section>
+      <section className="main"></section>      
     </div>
   );
 }
