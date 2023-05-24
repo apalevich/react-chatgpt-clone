bf7e22cb74ba05f61d9ca64c702ced26cbffd516
apalevich <backstageofme@gmail.com>
2023-05-24 17:57:24 +0400

    Add rest of stylization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <section className="side-bar">
         <button>+ New chat</button>
         <ul className="history"></ul>
@@ -8,7 +8,22 @@ function App() {
           <p>Made by Artyom</p>
         </nav>
       </section>
-      <section className="main"></section>      
+      <section className="main">
+        <h1>ArtyomGPT</h1>  
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">Send</div>
+          </div>
+          <p className="info">
+            Chat GPT. Free Research Preview.
+            Made as a student project and not for the business purpose.
+          </p>
+        </div>
+      </section>      
     </div>
   );
 }
