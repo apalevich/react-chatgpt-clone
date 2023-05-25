53f664c80dff99d5e0a7ce52472f65fa3f920474
apalevich <backstageofme@gmail.com>
2023-05-25 21:23:01 +0400

    Implement endpoint

---
+++ server.js
@@ -0,0 +1,37 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = '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';
+
+app.post('/completions', async (req, res) => {
+    const options = {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${API_KEY}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			model: 'gpt-3.5-turbo',
+			max_tokens: 100,
+			temperature: 0,
+			messages: [
+				{role: 'user', content: req.body.message }
+			]
+		})
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+        const data = await response.json();
+        res.send(data);
+    } catch (e) {
+        console.error(e);
+    }
+})
+
+app.listen(PORT, () => console.log(`Your server is running on port: ${PORT}`));
+
--- src/App.js
+++ src/App.js
@@ -1,4 +1,56 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState([]);
+
+  const getMessages = async () => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: value,
+      })
+    };
+
+    try {
+      const response = await fetch('http://localhost:8000/completions', options);
+      const data = await response.json();
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats(previousChats => {
+        return [
+          ...previousChats, 
+          {
+            title: currentTitle,
+            role: "user",
+            content: value,
+          },
+          {
+            title: currentTitle,
+            role: message.role,
+            content: message.content
+          }
+        ]
+      })
+    }
+  }, [message, currentTitle]);
+
+  console.log(previousChats);
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -9,14 +61,14 @@ function App() {
         </nav>
       </section>
       <section className="main">
-        <h1>ArtyomGPT</h1>  
+        <h1>{ currentTitle || 'ArtyomGPT' }</h1>  
         <ul className="feed">
 
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input/>
-            <div id="submit">Send</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>Send</div>
           </div>
           <p className="info">
             Chat GPT. Free Research Preview.
